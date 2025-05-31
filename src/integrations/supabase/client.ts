e7c77b0a1e354d5d35e15dd140435927b59af820
Solomon ABOYEJI <Solesty7@gmail.com>
2025-05-31 06:27:54 +0100

    feat: removed supabase secret from code

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,9 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://czdvdbxtmxmrhwfyicpn.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6ZHZkYnh0bXhtcmh3ZnlpY3BuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2NTk4MzUsImV4cCI6MjA2NDIzNTgzNX0.CLYbHXO52CAospfOX1g668eIcEFa0yFYKgFW8xlr1gg";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
+export const supabase = createClient<Database>(SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
